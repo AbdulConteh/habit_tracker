172e96fe80318b603fc91e8b49ca38319ce6483d
AbdulConteh <abdulrahmanwc@gmail.com>
2022-05-05 23:05:21 -0400

    adding azure blob and API

--- flask_app/controllers/images_controllers.py
+++ flask_app/controllers/images_controllers.py
@@ -1,8 +1,11 @@
+import os
+import requests 
+from flask import jsonify
 from flask_app import app
 from flask import redirect, session
+
+print( os.environ.get("FLASK_APP_API_KEY") )
 
 @app.route('/upload_img', methods=['POST'])
 def uploadImage():
     pass
-
-@app.route('')
--- server.py
+++ server.py
@@ -2,6 +2,9 @@ from flask_app import app
 from flask_app.controllers import users_controllers
 from flask_app.controllers import habits_controllers
 from flask_app.controllers import messages_controllers
+from dotenv import load_dotenv
+
+load_dotenv() 
 
 if __name__=="__main__":
     app.run(debug=True)
