960a93bcfdab9a69062b4d6636a643ef9fc861c9
AbdulConteh <abdulrahmanwc@gmail.com>
2022-05-06 23:22:37 -0400

    file upload w/ azure attempt 1

--- flask_app/controllers/images_controllers.py
+++ flask_app/controllers/images_controllers.py
@@ -1,10 +1,5 @@
-import os
-import requests 
-from flask import jsonify
 from flask_app import app
-from flask import redirect, session
-
-print( os.environ.get("FLASK_APP_API_KEY") )
+from flask import redirect, render_template
 
 @app.route('/upload_img', methods=['POST'])
 def uploadImage():
