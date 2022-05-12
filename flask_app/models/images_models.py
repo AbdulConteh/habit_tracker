d6f94882c9e33a5026ce8b2e365d0a170c478f3f
AbdulConteh <abdulrahmanwc@gmail.com>
2022-05-12 15:21:27 -0400

    css updateds to habit_tracker

--- flask_app/models/images_models.py
+++
@@ -1,9 +0,0 @@
-from flask_app.config.mysqlconnection import MySQLConnection
-db = "habit_tracker_db"
-
-class Images:
-    def __init__(self, data):
-        self.id = data['id']
-        self.name = data['name']
-        self.created_at = data['created_at']
-        self.updated_at = data['updated_at']
--- server.py
+++ server.py
@@ -2,6 +2,5 @@ from flask_app import app
 from flask_app.controllers import users_controllers
 from flask_app.controllers import habits_controllers
 from flask_app.controllers import messages_controllers
-from flask_app.controllers import images_controllers
 if __name__=="__main__":
     app.run(debug=True)
