478d11e3ef89a0d7efdd03b95996634bb8439560
AbdulConteh <abdulrahmanwc@gmail.com>
2022-05-15 22:20:21 -0400

    showing/hiding div

--- flask_app/static/script.js
+++ flask_app/static/script.js
@@ -1,15 +1,18 @@
 var table=document.querySelector('table')
 
-
-};
-
-async function getFitnessData() {
-    var response = await fetch(, options);
-    var fitnessData = await response.json();
-    var row=document.createElement('tr')
-    var data=document.createElement('td')
-    row.append(response)
-    return fitnessData;
+// async function getBMI() {
+//     var response = await fetch(, options);
+//     var fitnessData = await response.json();
+//     var row=document.createElement('tr')
+//     var data=document.createElement('td')
+//     row.append(response)
+//     return fitnessData;
+// }
+// console.log(getFitnessData());
+function minimize() {
+    document.getElementById("api_data").style.display = "none";
 }
-console.log(getFitnessData());
 
+function show() {
+    document.getElementById("api_data").style.display = "block";
+}
