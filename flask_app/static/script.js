a798e126c78dfb45a0a988e928af452513efa6d0
AbdulConteh <abdulrahmanwc@gmail.com>
2022-05-15 23:21:06 -0400

    securing application

--- flask_app/static/script.js
+++ flask_app/static/script.js
@@ -1,14 +1,19 @@
 var table=document.querySelector('table')
 
-// async function getBMI() {
-//     var response = await fetch(, options);
-//     var fitnessData = await response.json();
-//     var row=document.createElement('tr')
-//     var data=document.createElement('td')
-//     row.append(response)
-//     return fitnessData;
-// }
-// console.log(getFitnessData());
+FLASK_APP_API_KEY = process.env.FLASK_APP_API_KEY
+require('dotenv').config();
+
+
+async function getBMI() {
+    var response = await fetch(process.env.FLASK_APP_API_KEY, options);
+    var fitnessData = await response.json();
+    var row=document.createElement('tr')
+    var data=document.createElement('td')
+    row.append(response)
+    return fitnessData;
+}
+console.log(getFitnessData());
+
 function minimize() {
     document.getElementById("api_data").style.display = "none";
 }
--- server.py
+++ server.py
@@ -3,7 +3,5 @@ from flask_app.controllers import users_controllers
 from flask_app.controllers import habits_controllers
 from flask_app.controllers import messages_controllers
 
-# FLASK_APP_API_KEY = RANDOMAPIKEY
-
 if __name__=="__main__":
     app.run(debug=True)
